56e452a3f54fc3a409ca28e5029f2d4a8b0ce190
Ian Coppa <ian.coppa@intrepidlearning.com>
2026-05-31 09:24:11 -0700

    wip

--- src/routes/Flashcards.jsx
+++ src/routes/Flashcards.jsx
@@ -84,7 +84,8 @@ export default function Flashcards() {
     if (reverseOrder) result.reverse();
     return result;
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cards, selectedDate, reverseOrder, showFlaggedOnly]);
+
+}, [cards, selectedDate, reverseOrder, showFlaggedOnly]);
 
   const { deck, setDeck, index, setIndex, flipped, setFlipped } = useDeck(initialDeck);
 
